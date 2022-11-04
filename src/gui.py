690f5086908be25cd0c51de59b4f212f38651493
lil meow <uashsh3@gmail.com>
2022-11-04 22:28:42 +0530

    right yash fix

--- src/gui.py
+++ src/gui.py
@@ -31,34 +31,59 @@ screen = pygame.display.set_mode()
 # window.close() 
 
 
+# running = True
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+#     screen.fill((255,255,255))
+    # pygame.draw.circle(screen,(255,255,0),(250,250),75)
+pygame.display.set_caption('image')
+
+imp = pygame.image.load(r'./assets/board.png').convert_alpha()
+r1 = pygame.image.load(r'./assets/button_r (1).png').convert_alpha()
+r2 = pygame.image.load(r'./assets/button_r (2).png')
+r3 = pygame.image.load(r'./assets/button_r (3).png')
+r4 = pygame.image.load(r'./assets/button_r (4).png')
+r5 = pygame.image.load(r'./assets/button_r (5).png')
+r6 = pygame.image.load(r'./assets/button_r (6).png')
+r7 = pygame.image.load(r'./assets/button_r (9).png')
+class Button():
+      def __init__(self,image,x,y):
+          self.image = image
+          self.x = x
+          self.y = y
+          
+      def draw(self):
+       pos = pygame.mouse.get_pos()
+       if self.image.collide_point(pos) == 1:
+        #checking if pointer is clicking on the image
+        if pygame.mouse.get_pressed()[0] == 1:
+         running = False
+       screen.bilt(self.image , (self.x,self.y))
+
+sc = Button(imp,288 , 139)
+b1 =Button(r1,325 , 50)
 running = True
 while running:
+    sc.draw()
+    b1.draw()  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((255,255,255))
-    # pygame.draw.circle(screen,(255,255,0),(250,250),75)
-    pygame.display.set_caption('image')
- 
-    imp = pygame.image.load(r'./assets/board.png')
-    r1 = pygame.image.load(r'./assets/button_r (1).png')
-    r2 = pygame.image.load(r'./assets/button_r (2).png')
-    r3 = pygame.image.load(r'./assets/button_r (3).png')
-    r4 = pygame.image.load(r'./assets/button_r (4).png')
-    r5 = pygame.image.load(r'./assets/button_r (5).png')
-    r6 = pygame.image.load(r'./assets/button_r (6).png')
-    r7 = pygame.image.load(r'./assets/button_r (9).png')
     
     
-    screen.blit(imp, (288, 139))
-    screen.blit(r1, (325, 50))
-    screen.blit(r2, (430, 50))
-    screen.blit(r3, (535, 50))
-    screen.blit(r4, (640, 50))
-    screen.blit(r5, (745, 50))
-    screen.blit(r6, (850, 50))
-    screen.blit(r7, (955, 50))
-    pygame.display.flip()
+screen.blit(imp, (288, 139))
+#     screen.blit(r1, (325, 50))
+#     screen.blit(r2, (430, 50))
+#     screen.blit(r3, (535, 50))
+#     screen.blit(r4, (640, 50))
+#     screen.blit(r5, (745, 50))
+#     screen.blit(r6, (850, 50))
+#     screen.blit(r7, (955, 50))
+pygame.display.flip()
 pygame.quit()
 
