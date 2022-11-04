9b714be0b951409f09f944bf70da79efc29760da
lil meow <uashsh3@gmail.com>
2022-11-04 11:20:15 +0530

    yash op resturns

--- src/gui.py
+++ src/gui.py
@@ -18,9 +18,18 @@ the computation, and the solution,
 
 import pygame
 import game
-
+import PySimpleGUI as pg
 pygame.init()
 screen = pygame.display.set_mode()
+
+
+# layout = [[pg.Button('r1','center',visible=True)]]
+# window = pg.Window('layout',layout)
+# while True:
+#   event , values = window.read()
+#   if event == 'ok' or event == pg.WIN_CLOSED:
+#     break
+# window.close() 
 
 
 running = True
@@ -34,7 +43,23 @@ while running:
     pygame.display.set_caption('image')
  
     imp = pygame.image.load(r'./assets/board.png')
+    r1 = pygame.image.load(r'./assets/button_r (1).png')
+    r2 = pygame.image.load(r'./assets/button_r (2).png')
+    r3 = pygame.image.load(r'./assets/button_r (3).png')
+    r4 = pygame.image.load(r'./assets/button_r (4).png')
+    r5 = pygame.image.load(r'./assets/button_r (5).png')
+    r6 = pygame.image.load(r'./assets/button_r (6).png')
+    r7 = pygame.image.load(r'./assets/button_r (9).png')
+    
+    
     screen.blit(imp, (288, 139))
+    screen.blit(r1, (325, 50))
+    screen.blit(r2, (430, 50))
+    screen.blit(r3, (535, 50))
+    screen.blit(r4, (640, 50))
+    screen.blit(r5, (745, 50))
+    screen.blit(r6, (850, 50))
+    screen.blit(r7, (955, 50))
     pygame.display.flip()
 pygame.quit()
 
