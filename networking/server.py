6ac07199968051cef950a0548cf076286704f886
lil meow <uashsh3@gmail.com>
2022-11-25 06:51:24 +0530

    server and client patch

---
+++ networking/server.py
@@ -0,0 +1,32 @@
+import os 
+import sys 
+import socket
+from _thread import *
+
+server = ''
+port = 5555
+y = socket.socket(socket.AF_INET , socket.SOCK_STREAM) #af_inet is for ipv4 and sock_stream is tcp
+
+
+try:
+    y.bind((server,port))
+except socket.error as i:
+    str(i)
+
+y.listen(2)
+print('waiting for connections')
+def threading(connection):
+    while True:
+        inp = connection.recv(1500)
+        out = connection.decode('UTF-8')
+        if not inp:
+            print('no connection')
+            break
+        else:
+            print(out)
+            connection.sendall(str.encode(out))
+
+while True:
+    connection , addr = y.accept()
+    print('connected to: ',addr)
+    start_new_thread(threading , (connection,))
