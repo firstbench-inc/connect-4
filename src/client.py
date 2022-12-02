8cebae12c0fad41aa9ea0c54ae147012ac19ecaa
lil meow <uashsh3@gmail.com>
2022-12-02 10:51:08 +0530

    new

---
+++ src/client.py
@@ -0,0 +1,18 @@
+import socket
+
+
+
+class network():
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        self.server = '192.168.1.108'
+        self.port = 5555
+        self.addr = (self.server , self.port)
+        self.id = self.connect()
+    def connect(self):
+        self.client.connect(self.addr)
+        return self.client.recv(1500).decode()
+    def traffic(self,data):
+        self.client.send(str.encode(data ))
+        return self.client.recv(1500).decode()
+a = network()
