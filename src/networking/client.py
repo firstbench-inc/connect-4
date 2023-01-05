d5c51ead8b60c10b5534bdd33439ee886488f26f
lil meow <uashsh3@gmail.com>
2023-01-05 11:42:04 +0530

    sushrut

--- src/networking/client.py
+++ src/networking/client.py
@@ -5,7 +5,7 @@ from time import sleep
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.184.111"
+        self.server = "192.168.118.111"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
